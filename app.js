dd09ac20a08e1e64c43b2d12330063388f8f5dfa
Natalie Salemme <nataliesalemme@gmail.com>
2018-07-25 18:09:36 -0700

    Adjusting size depending on length of result

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const value = ('3.1415926535897932384626433832795028841971693993751058');
-const piAnswer = document.querySelector('#piResult');
+let piAnswer = document.querySelector('#piResult');
 let integer = document.querySelector('.int');
 let calculateButton = document.querySelector('#calculate');
 
 
 calculateButton.addEventListener('click', function() {
 //takes the substring starting at 3.1 so the value will be that number of numbers after the decimal point
-  console.log(value.substr(0, 2).concat(value.substr(2, integer.value)));
+  let result = (value.substr(0, 2).concat(value.substr(2, integer.value)));
+  console.log(result);
+  //update inner html to show the result
+  piAnswer.innerHTML = result;
 
+  if (result.length <= 10) {
+    document.getElementById('pi-circle').style.width = "180px";
+    document.getElementById('pi-circle').style.height = "180px";
+  } else if(result.length > 10 && result.length <= 20) {
+    document.getElementById('pi-circle').style.width = "250px";
+    document.getElementById('pi-circle').style.height = "250px";
+  } else if(result.length > 20 && result.length <= 30) {
+    document.getElementById('pi-circle').style.width = "420px";
+    document.getElementById('pi-circle').style.height = "420px";
+  } else if(result.length > 30 && result.length <= 40) {
+    document.getElementById('pi-circle').style.width = "600px";
+    document.getElementById('pi-circle').style.height = "600px";
+  }//bracket for calculate function
 });
